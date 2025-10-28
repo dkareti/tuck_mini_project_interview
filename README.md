0d46d11ff43ae4b09447331fd81fb4a98af7e569
Daniel Kareti <danielkareti@gmail.com>
2025-10-28 12:41:16 -0700

    Finished code within time limit

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 * The problem that I am trying to address is to provide an API interface that enables gather the 
 
 # Instructions to Run this Repo
+
+1. Run `python main.py`
+
+2. In another terminal, run curl commands to test the endpoints
+
+-  curl http://127.0.0.1:3001/getinfo
+-  curl 'http://127.0.0.1:3001/modifyinfo(text)'
+
--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 # This file will contain all the endpoints to manage the data
 from flask import Flask, request, render_template, jsonify
-
+from functions import access_field, modify_json, read_json
 app = Flask(__name__)
 
+#import the json output from the custom GPT
+JSON_FILE = './dev/test.json'
+
+#global data
+data = access_field()
+
 #define routes
-@app.route("/getinfo", methods=[])
+@app.route("/getinfo", methods=['GET'])
+def get_info():
+    '''
+    This function prints out the contents of 'gptOutput'.
+    '''
+
+    return data
+
+@app.route("/modify", methods=['POST'])
+def modify_info(new_string: str):
+    '''
+    This function updates the global 'getOutput' data.
+    '''
+    global data
+
+    json_info = read_json()
+
+    data = modify_json(JSON_FILE, new_string, json_obj=json_info)
+
 
 if __name__ == '__main__':
     #run the app on port 3001
